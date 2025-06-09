62b30234dd5b507303964e94c638f2ccf09a1d33
Esau Jesse <jesseesau13@gmail.com>
2025-06-09 11:20:52 +0100

    Implement simple interactive quiz

---
+++ quiz.js
@@ -0,0 +1,29 @@
+// function declaration
+function checkAnswer() {
+    // identify correct answer
+    const correctAnswer = "4";
+
+    // Retrieve the User's answer
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+    // ensure an option was selected
+    if (!selectedOption) {
+        document.getElementById("feedback").textContent = "Please select an answer."
+    }
+
+    const userAnswer = selectedOption.value;
+
+    // compare user's answer with correct answer
+    const feedback = document.getElementById("feedback");
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! well done.";
+        feedback.style.color = "#28a745" // green
+    } else {
+        feedback.textContent = "That's incorrect. Try again!"
+        feedback.style.color = "#dc3545" // red
+    }
+
+}
+
+// Add event listener
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
